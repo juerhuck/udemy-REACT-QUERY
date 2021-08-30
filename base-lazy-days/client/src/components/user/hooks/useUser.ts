76669f136b56b62d741b622e3cd57fa380cbc4db
Juergen Hucke <juergen.hucke@ima.de>
2021-08-30 11:23:25 +0200

    Mutations added

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from 'axios';
 import { useState } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 
@@ -10,12 +11,26 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-async function getUser(user: User | null): Promise<User | null> {
+interface AxiosResponseWithCancel extends AxiosResponse {
+  cancel: () => void;
+}
+
+async function getUser(user: User | null): Promise<AxiosResponseWithCancel> {
+  const source = axios.CancelToken.source();
   if (!user) return null;
-  const { data } = await axiosInstance.get(`/user/${user.id}`, {
-    headers: getJWTHeader(user),
-  });
-  return data.user;
+  const axiosResponse: AxiosResponseWithCancel = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+      cancelToken: source.token,
+    },
+  );
+
+  axiosResponse.cancel = () => {
+    source.cancel();
+  };
+
+  return axiosResponse;
 }
 
 interface UseUser {
@@ -31,7 +46,7 @@ export function useUser(): UseUser {
   // call useQuery to update user data from server
   useQuery(queryKeys.user, () => getUser(user), {
     enabled: !!user,
-    onSuccess: (data) => setUser(data),
+    onSuccess: (axiosResponse) => setUser(axiosResponse?.data?.user),
   });
 
   // meant to be called from useAuth
@@ -58,7 +73,7 @@ export function useUser(): UseUser {
     queryClient.setQueryData(queryKeys.user, null);
 
     // remove user appointments query
-    queryClient.removeQueries('user-appointments');
+    queryClient.removeQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return { user, updateUser, clearUser };
